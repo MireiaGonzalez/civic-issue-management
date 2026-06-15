920b54b1004aec4d75d7161fcb12215db0e71c64
Mireia Gonzalez <mireiagonzalezz03@gmail.com>
2026-06-16 01:56:46 +0200

    setup spring boot and docker container for posgresql

---
+++ backend/src/main/java/com/mireiagonzalez/urbanito/UrbanitoApplication.java
@@ -0,0 +1,13 @@
+package com.mireiagonzalez.urbanito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanitoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mireiagonzalez/urbanito/UrbanitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mireiagonzalez.urbanito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
